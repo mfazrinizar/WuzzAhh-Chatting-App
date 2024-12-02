26b411a876c351ac235aa8a2b76580b052e4ae32
M. Fazri Nizar <anon6372098.id@gmail.com>
2024-12-02 22:40:24 +0700

    Feat: MVC structure

---
+++ src/model/UserModel.java
@@ -0,0 +1,12 @@
+/*
+ * Author       : M. Fazri Nizar & Akram Ziyad Ramadhan
+ * Institution  : Sriwijaya University
+ * GitHub       : github.com/mfazrinizar (M. Fazri Nizar) & github.com/akam-kiko (Akram Ziyad Ramadhan)
+ * File Name    : UserModel.java
+ */
+
+package model;
+
+public class UserModel {
+    
+}
